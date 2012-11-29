c8598cc59a47bd682ef5cdd242992a73dbe02480
Michal Kawalec <mkawalec@lavabit.com>
2012-11-29 18:31:31 +0100

    Added more of the starting files.

---
+++ astronet/sql/00-database.sql
@@ -0,0 +1,4 @@
+-- The encoding part is for us to be able to accept other languages
+-- than the beloved english
+-- template0 is what must me inherited from in such a case.
+CREATE DATABASE astronet ENCODING 'UTF8' TEMPLATE template0;
---
+++ astronet/sql/10-users.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    -- User email - some additional constraints here
+    email varchar(50) UNIQUE,
+    -- The 'real' name of the user
+    real_name varchar(50),
+    -- User password
+    passwd varchar,
+    -- Salt, user-variable
+    salt varchar(10), 
+    disabled boolean DEFAULT FALSE,
+    -- Default locale for the user
+    -- Will be used for specifying search locale
+    locale varchar(10) DEFAULT 'pl',
+    -- This will define user's role
+    role varchar(5) DEFAULT 'user',
+);
