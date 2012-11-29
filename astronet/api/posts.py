717c80e26e9d37df9b3e89c4eecc5b29358ca314
Michal Kawalec <mkawalec@lavabit.com>
2012-11-29 20:03:46 +0100

    Posts schema changes and added API function for inserting posts.

---
+++ astronet/api/posts.py
@@ -0,0 +1,17 @@
+from ..api import api, auth_required, query_db
+from flask import (request, g, jsonify, abort)
+
+@api.route('/post', methods=['POST'])
+@auth_required
+def post():
+    # We want to save a post
+    if request.method == 'POST':
+        ret = query_db('INSERT INTO posts (author, title, lead, body) '
+                       'VALUES (%s, %s, %s, %s)',
+                       (g.uid, request.form['title'], request.form['lead'],
+                           request.form['body']))
+        if ret == 1:
+            return jsonify(status='succ')
+        elif ret == -1:
+            return jsonify(status='db_constraints_error')
+        return jsonify(status='db_error')
--- astronet/sql/20-posts.sql
+++ astronet/sql/20-posts.sql
@@ -2,7 +2,8 @@ CREATE TABLE posts (
     id BIGSERIAL PRIMARY KEY,
     author bigint REFERENCES users(id),
     title varchar(300) NOT NULL UNIQUE,
-    contents varchar,
+    lead varchar(500) NOT NULL UNIQUE,
+    body varchar,
     draft boolean DEFAULT FALSE,
 
     timestamp timestamp DEFAULT now()
