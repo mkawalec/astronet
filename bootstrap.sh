f4bc920b32445ffe437ac15d9e03f2ecc4fc67a6
Michal Kawalec <mkawalec@lavabit.com>
2012-11-30 15:06:12 +0100

    Added bootstrap script that should set everything up for new users.

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Installing packages\n"
+sudo pip2 install gevent flask sphinx psycopg2 pylibmc markdown
+
+echo "Setting up the database\n"
+cat astronet/sql/00-database.sql|psql -U postgres
+cat astronet/sql/*.sql|psql -U postgres -d astronet
+
+echo "Compiling coffescript\n"
+cd astronet && cake build && cd ..
+
+echo "You should now be able to start a server using 'python2 runserver.py'"
+echo "Thank you for using this fine utility"
