5ac63e0015294dd878969b9961efac9b31b66f80
Michal Kawalec <mkawalec@lavabit.com>
2012-11-30 15:09:16 +0100

    Removed superflous newlines

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Installing packages\n"
+echo "Installing packages"
 sudo pip2 install gevent flask sphinx psycopg2 pylibmc markdown
 
-echo "Setting up the database\n"
+echo "Setting up the database"
 echo "DROP DATABASE astronet"|psql -U postgres
 cat astronet/sql/00-database.sql|psql -U postgres
 cat astronet/sql/*.sql|psql -U postgres -d astronet
 
-echo "Compiling coffescript\n"
+echo "Compiling coffescript"
 cd astronet && cake build && cd ..
 
 echo "You should now be able to start a server using 'python2 runserver.py'"
