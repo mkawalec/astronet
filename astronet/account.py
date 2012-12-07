4f5eec5407d739926cc41acfd1834d7b04856379
Michal Kawalec <mkawalec@lavabit.com>
2012-12-07 15:41:17 +0100

    Style comment

--- astronet/account.py
+++ astronet/account.py
@@ -108,6 +108,8 @@ def reset_pass():
         if query_db('UPDATE users set reset_hash = %s WHERE email = %s',
         [gen_filename(10),request.form['email']]):
             # here one has to generate mail and send it TODO - configure mail server and check if it works
+            # Also, this must NOT go here. We need additional function in a different file
+            # that sends emails, otherwise we will just end up with a mess
             #sender = 'from@fromdomain.com'
             #receivers = ['asd@a.com']
             #message = """From: From Person <from@fromdomain.com>
