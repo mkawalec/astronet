d30e3a3af3553f9a029b70d02a3c70b4a047b9ce
Michal Kawalec <mkawalec@lavabit.com>
2012-12-08 17:26:58 +0100

    Changed images schema. The thumbnail doesn't need its own entry in the DB, we will want the tumbnails to conform to some kind of predectible naming scheme, so the image name will be 'fds32sA.jpg' and its thumbnail 'fds32sA_thumb.jpg' or something like that

--- astronet/sql/30-images.sql
+++ astronet/sql/30-images.sql
@@ -2,12 +2,12 @@ CREATE TABLE images (
     id BIGSERIAL PRIMARY KEY,
     author bigint REFERENCES users(id),
     title varchar(300) NOT NULL,
-    source varchar(300) NOT NULL,
-    description varchar(500) NOT NULL UNIQUE, --?
+    source varchar(600) NOT NULL, -- Increased the size, 
+    -- remember we will deal with google maps links:D
+    description varchar(500) NOT NULL, -- The description doesn't need to be unique
     body varchar,
 
 
     string_id varchar(12) NOT NULL UNIQUE, -- 'dorosły' obrazek
-    string_min_id varchar(12) NOT NULL UNIQUE, -- minaturka
     timestamp timestamp DEFAULT now()
 );
